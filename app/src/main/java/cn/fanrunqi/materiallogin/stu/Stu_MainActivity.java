280394e79f780623f11c5605a7cdacd6c4578c96
jinmutao <2549528898@qq.com>
2018-04-18 18:59:40 +0800

    Initial commit

---
+++ app/src/main/java/cn/fanrunqi/materiallogin/stu/Stu_MainActivity.java
@@ -0,0 +1,223 @@
+package cn.fanrunqi.materiallogin.stu;
+
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.transition.Explode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.fanrunqi.materiallogin.R;
+import io.codetail.animation.SupportAnimator;
+import io.codetail.animation.ViewAnimationUtils;
+import yalantis.com.sidemenu.interfaces.Resourceble;
+import yalantis.com.sidemenu.interfaces.ScreenShotable;
+import yalantis.com.sidemenu.model.SlideMenuItem;
+import yalantis.com.sidemenu.util.ViewAnimator;
+
+/**
+ * 学生用户进入时的主界面
+ */
+public class Stu_MainActivity extends AppCompatActivity implements ViewAnimator.ViewAnimatorListener{
+
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private List<SlideMenuItem> list = new ArrayList<>();
+    private ViewAnimator viewAnimator;
+    private LinearLayout linearLayout;
+    View view;
+    private final String TAG_HOME = "home";
+    private final String TAG_CLOSE = "close";
+    private final String TAG_NOTIFICATIONS = "notifications";
+    private final String TAG_SETTINGS = "settings";
+    private final String TAG_ABOUT = "about";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stu__main);
+
+        Test_fragment fragmentOne = Test_fragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, fragmentOne)
+                .commit();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
+        linearLayout = (LinearLayout) findViewById(R.id.left_drawer);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+        setActionBar();
+        createMenuList();
+        viewAnimator = new ViewAnimator<>(this, list, fragmentOne, drawerLayout, this);
+
+        Explode explode = new Explode();
+        explode.setDuration(500);
+        getWindow().setExitTransition(explode);
+        getWindow().setEnterTransition(explode);
+
+    }
+    private void createMenuList() {
+        SlideMenuItem menuItem = new SlideMenuItem(TAG_CLOSE, R.drawable.test_home);
+        list.add(menuItem);
+        SlideMenuItem menuItem1 = new SlideMenuItem(TAG_HOME, R.drawable.test_home);
+        list.add(menuItem1);
+        SlideMenuItem menuItem2 = new SlideMenuItem(TAG_NOTIFICATIONS, R.drawable.test_home);
+        list.add(menuItem2);
+        SlideMenuItem menuItem3 = new SlideMenuItem(TAG_SETTINGS, R.drawable.test_home);
+        list.add(menuItem3);
+        SlideMenuItem menuItem4 = new SlideMenuItem(TAG_ABOUT, R.drawable.test_home);
+        list.add(menuItem4);
+    }
+
+    private void setActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        drawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                linearLayout.removeAllViews();
+                linearLayout.invalidate();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                if (slideOffset > 0.6 && linearLayout.getChildCount() == 0)
+                    viewAnimator.showMenuContent();
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public ScreenShotable onSwitch(Resourceble slideMenuItem, ScreenShotable screenShotable, int position) {
+        switch (slideMenuItem.getName()) {
+            case "close":
+                return screenShotable;
+            default:
+                return replaceFragment(slideMenuItem,screenShotable, position);
+
+        }
+    }
+
+    private ScreenShotable replaceFragment(Resourceble slideMenuItem,ScreenShotable screenShotable, int topPosition) {
+        view = findViewById(R.id.content_frame);
+        int finalRadius = Math.max(view.getWidth(), view.getHeight());
+        SupportAnimator animator = ViewAnimationUtils.createCircularReveal(view, 0, topPosition, 0, finalRadius);
+        animator.setInterpolator(new AccelerateInterpolator());
+        animator.setDuration(ViewAnimator.CIRCULAR_REVEAL_ANIMATION_DURATION);
+        findViewById(R.id.content_overlay).setBackgroundDrawable(new BitmapDrawable(getResources(), screenShotable.getBitmap()));
+        animator.start();
+        Fragment fragment = null;
+        switch (slideMenuItem.getName()){
+            case TAG_HOME:
+                fragment  = Test_fragment.newInstance();
+                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment ).commit();
+                return (ScreenShotable) fragment;
+            case TAG_NOTIFICATIONS:
+                fragment = Test_fragment .newInstance();
+                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment ).commit();
+                return (ScreenShotable) fragment;
+            case TAG_SETTINGS:
+                fragment= Test_fragment .newInstance();
+                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment ).commit();
+                return (ScreenShotable) fragment;
+            case TAG_ABOUT:
+                fragment = Test_fragment.newInstance();
+                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+                return (ScreenShotable) fragment;
+            default:
+                break;
+        }
+        return (ScreenShotable) fragment;
+    }
+
+
+    @Override
+    public void disableHomeButton() {
+        getSupportActionBar().setHomeButtonEnabled(false);
+
+    }
+
+    @Override
+    public void enableHomeButton() {
+        getSupportActionBar().setHomeButtonEnabled(true);
+        drawerLayout.closeDrawers();
+
+    }
+
+    @Override
+    public void addViewToContainer(View view) {
+        linearLayout.addView(view);
+    }
+
+
+}
---
+++ app/src/main/java/cn/fanrunqi/materiallogin/stu/Test_fragment.java
@@ -0,0 +1,68 @@
+package cn.fanrunqi.materiallogin.stu;
+
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.transition.Explode;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import junit.framework.Test;
+
+import cn.fanrunqi.materiallogin.R;
+import yalantis.com.sidemenu.interfaces.ScreenShotable;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Test_fragment extends Fragment implements ScreenShotable {
+
+
+    private View Fragmentone_view;
+    private Bitmap bitmap;
+    public Test_fragment() {
+        // Required empty public constructor
+    }
+
+    public static Test_fragment newInstance(){
+        return new Test_fragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.test_fragment, container, false);
+    }
+
+    @Override
+    public void takeScreenShot() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Bitmap bitmap = Bitmap.createBitmap(Fragmentone_view.getWidth(),
+                        Fragmentone_view.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                Fragmentone_view.draw(canvas);
+                Test_fragment.this.bitmap = bitmap;
+            }
+        };
+
+        thread.start();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.Fragmentone_view = view.findViewById(R.id.container);
+    }
+    @Override
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+}
